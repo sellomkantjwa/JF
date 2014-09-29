e566d13a4865cb4709cd99798a6e75e08e3d2e56
giza182 <giza182@gmail.com>
2014-09-29 10:48:44 +0200

    added a connection manager

--- JFMVC/Controllers/HomeController.cs
+++ JFMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,18 @@ namespace JFMVC.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            Utils.DBConnectionManager db = new Utils.DBConnectionManager();
 
-            return View();
+
+            if (db.OpenConnection())
+            {
+                string myInsertQuery = "INSERT INTO Orders (id, customerId, amount) Values(1001, 23, 30.66)";
+                db.InsertRow();
+                db.CloseConnection();
+                return View();
+            }
+
+            return null;
         }
 
         public ActionResult About()
---
+++ JFMVC/Util/DBConnectionManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using MySql.Web;
+
+
+namespace JFMVC.Utils
+{
+
+
+    public class DBConnectionManager
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnectionManager()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "jobfair";
+            uid = "root";
+            password = "Ningiza";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+
+                        break;
+
+                    case 1045:
+
+                        break;
+                }
+                return false;
+            }
+        }
+
+
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                string message = ex.ToString();
+                return false;
+            }
+        }
+
+
+        public void InsertRow()
+        {
+            string myInsertQuery = "INSERT INTO Employer (email, address, password, name, contactNumber) VALUES ('giza182@gmail', '111 joburg', '123456789', 'sello', '0113332256')";
+            MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+            myCommand.Connection = connection;
+            myCommand.ExecuteNonQuery();
+            myCommand.Connection.Close();
+        }
+    }
+
+
+}
